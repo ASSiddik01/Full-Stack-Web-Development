7dfb19a1d2222dcbd03449a55045c6f0df16a6ae
Shama <assiddik001@gmail.com>
2021-07-31 18:20:42 +0600

    m-16 v-4: variable name declear method

---
+++ Milestone_4/Module_16/variable_name.js
@@ -0,0 +1,19 @@
+/*
+In variable name
+--------------------
+Reserved key not allowed 
+quotation is not allowed
+hyphen is not allowed
+first letter is always an alphabet
+Under_score is allowed but camelCase is good practices
+*/
+
+// var false = 43; //Reserved key
+
+// var 'name' = "shama"; //quotation
+
+// var last-name = "shama"; //hyphen
+
+// var 99pricce = "99"; // not alphabet
+
+var myFullName= "Abu Shama"; //camelCase
