1f780dcbb247c1eb3ae2166b2000bc28e41533fc
Shama <assiddik001@gmail.com>
2021-08-01 00:40:55 +0600

    m-17 v-4: Array index and set value by index

---
+++ Milestone_4/Module_17/array_index.js
@@ -0,0 +1,10 @@
+var number = [33, 32, 34, 134, 44, 24]
+
+console.log(number.indexOf(32)); //find index
+
+console.log(number[1]); // find value
+
+console.log(number);
+
+number[1] = 66;
+console.log(number);
