7817af12bf175a7739dcd80c81a1ab278b131676
Shama <assiddik001@gmail.com>
2021-08-01 01:05:37 +0600

    m-17 v-4:Element add or remove in array

---
+++ Milestone_4/Module_17/push_pop.js
@@ -0,0 +1,16 @@
+var number = [33, 32, 34, 44, 24]
+console.log(number);
+
+number.push(55); //add element in last
+console.log(number);
+
+number.pop(); //remove element in last
+console.log(number);
+
+number.unshift(63); //add element in last
+console.log(number);
+
+number.shift(); //remove element in last
+console.log(number);
+
+
