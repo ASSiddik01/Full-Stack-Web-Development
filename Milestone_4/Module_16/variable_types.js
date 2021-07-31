d628012ca9283a232de1aed9d45571cb9a98c104
Shama <assiddik001@gmail.com>
2021-07-31 21:18:28 +0600

    m-16 v-8: Check variable types and after float toFixed

---
+++ Milestone_4/Module_16/variable_types.js
@@ -0,0 +1,16 @@
+var age = 25;
+var name = 'Shama';
+var israinning = true
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof israinning);
+
+var amount1 = 20.33;
+var amount2 = 4.666666;
+var sum = amount1 = amount2;
+sum = sum.toFixed(2);
+sum = parseFloat(sum);
+
+console.log(sum);
+console.log(typeof sum);
