a8c65104ead1a7e81f020e0a2f7271d79fb94a72
Shama <assiddik001@gmail.com>
2021-07-31 18:41:08 +0600

    m-16 v-5: Mathmatical Operation in JS

---
+++ Milestone_4/Module_16/math_operation.js
@@ -0,0 +1,16 @@
+var lemonJuice = 12;
+var orangeJuice = 15;
+var juiceQuantity = 5;
+var totalMoney = 100;
+
+var add = lemonJuice+orangeJuice;
+var subustraction = lemonJuice-orangeJuice;
+var multipication = juiceQuantity*orangeJuice;
+var divided = totalMoney/lemonJuice;
+
+console.log(lemonJuice);
+console.log(orangeJuice);
+console.log(add);
+console.log(subustraction);
+console.log(multipication);
+console.log(divided);
