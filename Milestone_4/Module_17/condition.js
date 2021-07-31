73f857d5b391c514b3b0deb0f55d0046aedd8f5c
Shama <assiddik001@gmail.com>
2021-08-01 01:48:58 +0600

    m-17 v-6: condition check by if else

---
+++ Milestone_4/Module_17/condition.js
@@ -0,0 +1,10 @@
+var number1 = 77;
+var number2 = 77;
+
+if (number1 > number2) {
+    console.log("Number 1 is gratter then number 2");
+} else if(number1 == number2){
+    console.log("Both number are same");
+} else {
+    console.log("Number 1 is smaller is grater then number 2")
+}
