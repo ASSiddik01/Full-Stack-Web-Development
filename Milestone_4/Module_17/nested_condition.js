b5889cc39656be4a9fd6bcf417550cb01cbb9c79
Shama <assiddik001@gmail.com>
2021-08-01 18:07:53 +0600

    m-17 v-8: Nested condition check

---
+++ Milestone_4/Module_17/nested_condition.js
@@ -0,0 +1,15 @@
+var gotJob = true;
+var hasMoney = 100000;
+var hasFlat = true;
+var hasHouse = true;
+
+
+if (gotjob == true) {
+    if (hasFlat == true || hasHouse == true) {
+        console.log("Biya Confirm");
+    } else {
+        console.log("Flat ba Bari age koro tar por biya koiro");
+    }
+} else {
+    console.log("Biya Cancel");
+}
