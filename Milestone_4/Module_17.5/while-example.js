f5a0f377875a8a0e094464a43aec1ce665255041
Shama <assiddik001@gmail.com>
2021-08-02 18:44:30 +0600

    m-17.5 v-3: even and odd number

---
+++ Milestone_4/Module_17.5/while-example.js
@@ -0,0 +1,18 @@
+// Even
+
+var number = 0;
+
+while (number <= 20) {
+    console.log(number);
+    number += 2;
+}
+
+
+// Odd
+var number = 1;
+
+while (number <= 20) {
+    console.log(number);
+    number += 2;
+}
+
