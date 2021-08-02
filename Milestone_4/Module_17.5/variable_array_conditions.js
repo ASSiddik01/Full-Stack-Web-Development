ac0108ba11021993d53cece9c656bd1ae376ccef
Shama <assiddik001@gmail.com>
2021-08-02 18:16:55 +0600

    m-17.5 v-1: Recape variable, array and condition

---
+++ Milestone_4/Module_17.5/variable_array_conditions.js
@@ -0,0 +1,36 @@
+var bottolColor = "yellow"; //string
+console.log(bottolColor);
+
+var bottolCapacity = 1;  //number
+console.log(bottolCapacity);
+
+var isFull = true; //boolean
+console.log(isFull);
+
+// Array
+var items = ["Pen", "Mobile", "Money bag"]
+
+console.log(items.indexOf("Mobile")); //find index
+
+console.log(items); //print array
+
+items.push("Visiting Card"); //add element in last
+console.log(items);
+
+items.shift("Visiting Card"); // remove element from first
+console.log(items);
+
+console.log(items.length); // count the element
+
+console.log(items[2]); //find the element;
+
+
+//condition 
+
+var hasMoney = true;
+
+if (hasMoney == true) {
+    console.log("all ok");
+} else {
+    console.log("Vai taka Poisa nai re");
+}
