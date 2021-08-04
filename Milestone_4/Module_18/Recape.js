c17339ee0cf2d398626d7bc3985f655d6cbdafb8
Shama <assiddik001@gmail.com>
2021-08-04 18:01:19 +0600

    m-18 v-1: array, variable, loop,condition recape

---
+++ Milestone_4/Module_18/Recape.js
@@ -0,0 +1,54 @@
+var bookName = "7 days";
+
+var bookList = ["Soishob", "Latai ghuri", "Amader gram"];
+console.log(bookList);
+console.log("\n");
+
+console.log(bookList.length); //lenght check
+console.log("\n");
+
+
+console.log(typeof bookList);
+console.log("\n");
+
+
+bookList.unshift('Khureghor');
+console.log(bookList);
+console.log("\n");
+
+
+bookList.pop()
+console.log(bookList);
+console.log("\n");
+
+
+console.log(bookList.indexOf("Soishob"));
+console.log("\n");
+
+console.log(bookList[2]);
+console.log("\n");
+
+// Conditionals
+if (bookName = "7 Days") {
+    console.log("This book name is 7 days");
+} else {
+    console.log("I don't know this book name");
+}
+console.log("\n");
+
+// While Loop
+var number = 0;
+
+while (number <= 10) {
+    console.log(number);
+    number++;
+}
+console.log("\n");
+
+// For Loop
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+console.log("\n");
+
+
