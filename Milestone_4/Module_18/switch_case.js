af714643125e01c5959a6ff356eccbf8a319d65a
Shama <assiddik001@gmail.com>
2021-08-04 22:46:27 +0600

    m-18 v-7: How to write switch case

---
+++ Milestone_4/Module_18/switch_case.js
@@ -0,0 +1,15 @@
+var color = "black";
+
+switch (color){
+    case 'red':
+        console.log("This color is Red");
+        break;
+    case 'green':
+        console.log("This color is Green");
+        break;
+    case 'blue':
+        console.log("This color is Blue");
+        break;
+    default:
+        console.log("This color is ",color);
+}
