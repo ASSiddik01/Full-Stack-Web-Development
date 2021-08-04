c0e589ab6030f29d845b56e54f5841a608b633c7
Shama <assiddik001@gmail.com>
2021-08-04 21:46:56 +0600

    m-18 v-3: Function er perameter and return

---
+++ Milestone_4/Module_18/function parametter.js
@@ -0,0 +1,13 @@
+function juiceKhabo(taka){
+    console.log("Mama Juice den");
+    console.log("Ai nen taka",taka);
+    var juicePrice=10;
+    var juiceQuentity= taka/juicePrice;
+    return juiceQuentity;
+
+}
+
+var money =200;
+
+var juice = juiceKhabo(money)
+console.log("Ai now Juice",juice,"ta");
