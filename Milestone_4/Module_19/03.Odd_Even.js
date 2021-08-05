e2f1e6d953be62b1abfc823377ac32cfbc3099a4
Shama <assiddik001@gmail.com>
2021-08-05 07:18:39 +0600

    m-19 v-3: Odd Even check

---
+++ Milestone_4/Module_19/03.Odd_Even.js
@@ -0,0 +1,9 @@
+function EvenOrOdd(number) {
+    if (number % 2 == 0) {
+        console.log(number, 'is a even number');
+    } else {
+        console.log(number, "is a odd number");
+    }
+}
+
+EvenOrOdd(10);
