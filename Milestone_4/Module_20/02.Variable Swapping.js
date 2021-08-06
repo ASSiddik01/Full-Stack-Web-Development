671f25f363a8c5ba5b5047bad2f50a604d40631c
Shama <assiddik001@gmail.com>
2021-08-06 18:27:28 +0600

    m-20 v-2: Destructuring Method

---
+++ Milestone_4/Module_20/02.Variable Swapping.js
@@ -0,0 +1,14 @@
+var first = 2;
+var second=4;
+console.log(first,second);
+
+// Temp Method
+// var temp = first;
+// first=second;
+// second=temp;
+// console.log(first,second);
+
+// Destructuring Method
+
+[first,second]=[second,first];
+console.log(first,second);
