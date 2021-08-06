91dddb105b68c1989bac43f25cb2879dd1fffe23
Shama <assiddik001@gmail.com>
2021-08-06 18:17:16 +0600

    m-20 v-1: Find diffrent type of value from math

---
+++ Milestone_4/Module_20/01.Math.js
@@ -0,0 +1,22 @@
+console.log("Absulate Value");
+var myNumbar = -7.889;
+var absulateValue = Math.abs(myNumbar);
+console.log(absulateValue);
+
+console.log("Ceiling Value");
+var myNumbar = 7.289;
+var ceilingValue = Math.ceil(myNumbar);
+console.log(ceilingValue);
+
+console.log("Floor Value");
+var myNumbar = 7.289;
+var floorValue = Math.floor(myNumbar);
+console.log(floorValue);
+
+console.log("Round Value");
+var myNumbar = 7.589;
+var roundValue = Math.round(myNumbar);
+console.log(roundValue);
+
+console.log("Random Value");
+console.log(Math.random() * 10);
