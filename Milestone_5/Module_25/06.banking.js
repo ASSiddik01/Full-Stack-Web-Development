87488a243c713ceca19d3580c73607265442377a
Shama <assiddik001@gmail.com>
2021-08-16 22:49:15 +0600

    m-25 v-7: Update deposit and balance

--- Milestone_5/Module_25/06.banking.js
+++ Milestone_5/Module_25/06.banking.js
@@ -1,6 +1,20 @@
 document.getElementById('depositeButton').addEventListener('click',function(){
     const amount = document.getElementById('depositeAmount');
+    const inputAmount = parseFloat(amount.value);
+
     const depositeTotal = document.getElementById('depositeMoney');
-    depositeTotal.innerText = amount.value;
-    amount.value= "";
+    const oldAmount = parseFloat(depositeTotal.innerText);
+    
+    const totalAmount = inputAmount + oldAmount;
+    depositeTotal.innerText = totalAmount;
+
+    amount.value = "";
+
+    // Update total balance
+    const balanceTotal = document.getElementById('totalMoney');
+    const balnceAmount = parseFloat(balanceTotal.innerText);
+
+    const newTotalBalance = balnceAmount+inputAmount;
+    balanceTotal.innerText = newTotalBalance;
+
 })
