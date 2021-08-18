2357a804e2bd33f7cf37756c02ee2dc58dd9e235
Shama <assiddik001@gmail.com>
2021-08-18 21:00:07 +0600

    m-27 v-2: Case price update

--- Milestone_5/Module_27/app.js
+++ Milestone_5/Module_27/app.js
@@ -1,6 +1,24 @@
-document.getElementById('case_plus').addEventListener('click',function(){
-    const caseInput =  document.getElementById('case_number')
-    const caseNumber = caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
+function updateCaseNumber(isIncrease) {
+    const caseInput = document.getElementById('case_number')
+    let caseNumber = caseInput.value;
+    if (isIncrease == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    } else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber
 
+    const caseTotal = document.getElementById('case_total');
+    caseTotal.innerText = caseNumber *59
+
+}
+
+
+document.getElementById('case_plus').addEventListener('click', function () {
+    updateCaseNumber(true);
+
+})
+
+document.getElementById('case_minus').addEventListener('click', function () {
+    updateCaseNumber(false);
 })
