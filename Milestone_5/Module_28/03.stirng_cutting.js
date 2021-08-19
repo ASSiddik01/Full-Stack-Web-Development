8ad6db1daf53fd34c61f8ee3702a28267759a873
Shama <assiddik001@gmail.com>
2021-08-19 19:43:51 +0600

    m-28 v-3: String devied and join

---
+++ Milestone_5/Module_28/03.stirng_cutting.js
@@ -0,0 +1,23 @@
+const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, voluptates obcaecati"
+
+const split = text.split(' ');
+// console.log(split);
+
+const slice = text.slice(6, 20);
+// console.log(slice);
+
+const substr = text.substr(6, 11);
+// console.log(substr);
+
+const substring = text.substring(6, 20);
+// console.log(substring);
+
+const firstName = "Abu";
+const lastName = "Shama";
+const concat = firstName.concat(lastName);
+// console.log(concat);
+
+const words = ['My', 'name', 'is', 'Shama']
+const join = words.join(", ")
+console.log(join);
+
