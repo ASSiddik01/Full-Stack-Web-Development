5818ed915916201b4ef8cb7c438760b85a8a91bf
Shama <assiddik001@gmail.com>
2021-08-19 06:16:39 +0600

    m-27 v-4:Total price update

--- Milestone_5/Module_27/app.js
+++ Milestone_5/Module_27/app.js
@@ -11,7 +11,29 @@ function updateCaseNumber(product, isIncrease, price) {
     const caseTotal = document.getElementById(product + '_total');
     caseTotal.innerText = caseNumber * price;
 
+    // calculate total
+
+    calculationTotal();
+
 }
+function getInputValue(product) {
+    const productInput = document.getElementById(product + "_number");
+    const procuctNumber = parseInt(productInput.value);
+    return procuctNumber;
+}
+
+function calculationTotal() {
+    const phoneTotal = getInputValue("phone") * 1200;
+    const caseTotal = getInputValue("case") * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const taxAmount = subTotal / 10;
+    const totalPrice = subTotal + taxAmount
+    document.getElementById('sub_total').innerText = subTotal;
+    document.getElementById('tax_amount').innerText = taxAmount;
+    document.getElementById('total_price').innerText = totalPrice;
+}
+
+
 // Phone Events
 document.getElementById('phone_plus').addEventListener('click', function () {
     updateCaseNumber('phone', true, 1200);
