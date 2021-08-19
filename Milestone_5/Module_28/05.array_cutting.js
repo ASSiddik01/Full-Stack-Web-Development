5408cae47d44df8435e6f656b0debfff254797ea
Shama <assiddik001@gmail.com>
2021-08-19 20:18:00 +0600

    m-28 v-5: Array parts

---
+++ Milestone_5/Module_28/05.array_cutting.js
@@ -0,0 +1,9 @@
+const numbers = [3,5,2,55,63,62,33,44,52,88,76];
+
+// const slice =  numbers.slice(3,6);
+// console.log(slice);
+// console.log(numbers);
+
+const splice = numbers.splice(3,3,11,22);
+console.log(splice);
+console.log(numbers);
