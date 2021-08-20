4296f960f47d5b9da35b5cf084f4774e986721d7
Shama <assiddik001@gmail.com>
2021-08-20 12:40:52 +0600

    list create and add button

--- Milestone_5/Module_28.5/leader-board-css3/script.js
+++ Milestone_5/Module_28.5/leader-board-css3/script.js
@@ -10,3 +10,13 @@ playerBg.onclick = redRGBA();
 function redRGBA(){
     document.getElementById('top-players').style.backgroundColor = "rgba(255, 0, 0, 0.25)";
 }
+
+// list create
+const getButton = document.getElementById('createButton').addEventListener('click',function(){
+
+    const li = document.createElement('li');
+    li.innerText = "New list";
+
+    const newList = document.getElementById('list');
+    newList.appendChild(li);
+})
