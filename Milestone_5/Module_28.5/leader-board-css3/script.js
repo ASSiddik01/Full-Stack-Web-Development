46a83609c06194f3085ce48664d29fdca5909341
Shama <assiddik001@gmail.com>
2021-08-20 12:57:33 +0600

    input field and increase field

--- Milestone_5/Module_28.5/leader-board-css3/script.js
+++ Milestone_5/Module_28.5/leader-board-css3/script.js
@@ -7,16 +7,26 @@ function colorChange(title) {
 const playerBg = document.getElementById('top-players');
 playerBg.onclick = redRGBA();
 
-function redRGBA(){
+function redRGBA() {
     document.getElementById('top-players').style.backgroundColor = "rgba(255, 0, 0, 0.25)";
 }
 
 // list create
-const getButton = document.getElementById('createButton').addEventListener('click',function(){
+const getButton = document.getElementById('createButton').addEventListener('click', function () {
 
     const li = document.createElement('li');
     li.innerText = "New list";
 
     const newList = document.getElementById('list');
     newList.appendChild(li);
+})
+
+// Input value increase
+
+document.getElementById('plus_button').addEventListener('click', function () {
+    const inputNumber = document.getElementById('input');
+    const inputValue = inputNumber.value;
+    const newValue = parseInt(inputValue) + 1;
+    inputNumber.value = newValue;
+
 })
