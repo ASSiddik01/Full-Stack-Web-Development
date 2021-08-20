7f2277b312f8c95fef49f150c2417fef4cba25d9
Shama <assiddik001@gmail.com>
2021-08-20 12:17:29 +0600

    player color change

--- Milestone_5/Module_28.5/leader-board-css3/script.js
+++ Milestone_5/Module_28.5/leader-board-css3/script.js
@@ -1,3 +1,12 @@
+// Title color
 function colorChange(title) {
-    document.getElementById(title +"_title").style.color = "red";
-  }
+    document.getElementById(title + "_title").style.color = "red";
+}
+
+// Player BG
+const playerBg = document.getElementById('top-players');
+playerBg.onclick = redRGBA();
+
+function redRGBA(){
+    document.getElementById('top-players').style.backgroundColor = "rgba(255, 0, 0, 0.25)";
+}
