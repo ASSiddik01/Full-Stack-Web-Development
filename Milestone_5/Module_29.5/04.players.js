04466626ba1b2495d0b645001dc9fc54522ec422
Shama <assiddik001@gmail.com>
2021-08-23 09:56:41 +0600

    m-29.5 v-4: Players style add by js

---
+++ Milestone_5/Module_29.5/04.players.js
@@ -0,0 +1,12 @@
+const players = document.getElementsByClassName('player');
+
+for (const player of players) {
+    player.style.border = "1px solid red";
+    player.style.borderRadius = "10px";
+    player.style.margin = "10px";
+    player.style.padding = "10px";
+}
+
+function addPlayers() {
+    
+}
