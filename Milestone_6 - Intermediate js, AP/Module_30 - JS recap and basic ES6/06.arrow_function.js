ca46a69ad51c87a361a8e7224bbf0cb1aa86bff4
Shama <assiddik001@gmail.com>
2021-08-23 22:42:53 +0600

    m-30 v-7: How to declear big arrow function

--- Milestone_6 - Intermediate js, AP/Module_30 - JS recap and basic ES6/06.arrow_function.js
+++ Milestone_6 - Intermediate js, AP/Module_30 - JS recap and basic ES6/06.arrow_function.js
@@ -28,3 +28,4 @@ const sum2 = add2(15, 10);
 const sum3 = add3(15, 10);
 const sum4 = add4(15, 10);
 console.log(sum1, sum2, sum3, sum4);
+
---
+++ Milestone_6 - Intermediate js, AP/Module_30 - JS recap and basic ES6/07.big_arrow_functions.js
@@ -0,0 +1,8 @@
+const doMath = (x,y) => {
+    const sum = x+y;
+    const sub = x-y;
+    const result = sum*sub;
+    return result;
+}
+
+console.log(doMath(7,5));
