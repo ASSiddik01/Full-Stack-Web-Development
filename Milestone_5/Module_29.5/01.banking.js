d9a8757cfdab44ae042b74805ee7df55d02bf54f
Shama <assiddik001@gmail.com>
2021-08-23 09:07:28 +0600

    m-29.5 v-1: Deposite filed update

---
+++ Milestone_5/Module_29.5/01.banking.js
@@ -0,0 +1,51 @@
+function getInputValue(inputId) {
+    const amount = document.getElementById(inputId);
+    const inputAmount = parseFloat(amount.value);
+    // Clean deposite input value
+    amount.value = "";
+    return inputAmount;
+}
+
+function updateTotalFiled(totalFiledId, inputAmount) {
+    const depositeTotal = document.getElementById(totalFiledId);
+    const oldAmount = parseFloat(depositeTotal.innerText);
+
+    const totalAmount = inputAmount + oldAmount;
+    depositeTotal.innerText = totalAmount;
+}
+
+// function updateBlance(inputAmount, isAdd) {
+//     const balanceTotal = document.getElementById('totalMoney');
+//     const balnceAmount = parseFloat(balanceTotal.innerText);
+
+//     if (isAdd == true) {
+//         const newTotalBalance = balnceAmount + inputAmount;
+//         balanceTotal.innerText = newTotalBalance;
+//     } else {
+//         const newTotalBalance = balnceAmount - inputAmount;
+//         balanceTotal.innerText = newTotalBalance;
+//     }
+// }
+
+document.getElementById('depositeButton').addEventListener('click', function () {
+    const inputAmount = getInputValue('depositeAmount');
+    if (inputAmount > 0) {
+        updateTotalFiled('depositeMoney', inputAmount)
+        updateBlance(inputAmount, true);
+    }
+
+})
+
+// // Withdrow
+
+// document.getElementById('withdrowButton').addEventListener('click', function () {
+//     const newdrowAmount = getInputValue('withdrowAmount');
+
+//     if (newdrowAmount > 0) {
+        
+//         updateTotalFiled('withdrowMoney', newdrowAmount)
+//         updateBlance(newdrowAmount, false);
+//     }
+
+
+// })
