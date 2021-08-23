8b0a0fc038a11abfbc2fe9005ac14c17b01b1b14
Shama <assiddik001@gmail.com>
2021-08-23 10:21:36 +0600

    m-29.5 v-5: Add Event by event bubble

--- Milestone_5/Module_29.5/04.players.js
+++ Milestone_5/Module_29.5/04.players.js
@@ -1,12 +1,32 @@
-const players = document.getElementsByClassName('player');
-
-for (const player of players) {
+function setPlayersStyle(player) {
     player.style.border = "1px solid red";
     player.style.borderRadius = "10px";
     player.style.margin = "10px";
     player.style.padding = "10px";
 }
 
-function addPlayers() {
-    
+const players = document.getElementsByClassName('player');
+
+for (const player of players) {
+    setPlayersStyle(player);
 }
+
+function addPlayers() {
+    const playerContainer = document.getElementById('players');
+    const player = document.createElement('div');
+    player.classList.add('player');
+    player.innerHTML = `
+    <h4 class="player_name">New Player</h4>
+    <p>Quisquam ullam reprehenderit aliquid inventore sed quaerat dolore dicta vero similique optio aut provident quas odio, cum, laborum explicabo dolor, veniam eligendi harum distinctio. Ea temporibus possimus aliquid pariatur minima?</p>
+    `
+    setPlayersStyle(player);
+    playerContainer.appendChild(player);
+}
+
+document.getElementById('players').addEventListener('click', function (event) {
+    if (event.target.tagName.toLowerCase() == "div") {
+        event.target.style.backgroundColor = "yellow";
+    } else{
+        event.target.parentNode.style.backgroundColor = "yellow";
+    }
+})
