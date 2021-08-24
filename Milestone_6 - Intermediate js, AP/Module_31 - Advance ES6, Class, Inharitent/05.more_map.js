21eb5da44bd16e5db865d4482662488a869027d2
Shama <assiddik001@gmail.com>
2021-08-24 13:56:59 +0600

    m-31 v-5: Uses map and forEach

---
+++ Milestone_6 - Intermediate js, AP/Module_31 - Advance ES6, Class, Inharitent/05.more_map.js
@@ -0,0 +1,11 @@
+const products = [
+    {name:'iphone 6',price: 46000, color:'black'},
+    {name:'watch',price: 150, color:'black'},
+    {name:'Canon camera',price: 5000, color:'black'},
+]
+
+// let productName = products.map(product=>product.name);
+// let productName = products.map(product=>product);
+// console.log(productName);
+
+products.forEach(product=>console.log(product));
