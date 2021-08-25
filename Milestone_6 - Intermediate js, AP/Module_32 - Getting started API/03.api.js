858987850029a5f7d78ec5335eb9a3a42d348e54
Shama <assiddik001@gmail.com>
2021-08-26 00:22:14 +0600

    m-32 v-4: JSON data load form server with more examples

--- Milestone_6 - Intermediate js, AP/Module_32 - Getting started API/03.api.js
+++ Milestone_6 - Intermediate js, AP/Module_32 - Getting started API/03.api.js
@@ -3,3 +3,19 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(json => console.log(json))
 }
+
+function users() {
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => display(data))
+}
+
+function photos() {
+fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+function display(data) {
+    console.log(data);
+}
