a87603d82d82a5cfe546d97cf393f9044993ee2d
Shama <assiddik001@gmail.com>
2021-08-27 04:25:51 +0600

    m-32 v-4: Display dynamic data about country

--- Milestone_6 - Intermediate js, AP/Module_33 - API examplaes with edge cases/03.countries.js
+++ Milestone_6 - Intermediate js, AP/Module_33 - API examplaes with edge cases/03.countries.js
@@ -11,12 +11,30 @@ const displayCountries = countries => {
         // console.log(country)
         const div = document.createElement('div');
         div.classList.add('country')
-        const h3 = document.createElement('h3')
-        h3.innerText = country.name;
-        const p = document.createElement('p')
-        p.innerText = country.capital;
-        div.appendChild(h3);
-        div.appendChild(p);
+        div.innerHTML = `
+            <h3>${country.name}</h3>
+            <p>${country.capital}</p>
+            <button onclick="loadDetiles('${country.name}')">Detiles</button>
+        `;
         countriesContainer.appendChild(div);
     })
+}
+
+const loadDetiles = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetiles(data[0]))
+    
+}
+
+const displayDetiles = country => {
+    // console.log(country);
+    const detaileDiv = document.getElementById('detailes');
+    detaileDiv.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>Currency: ${country.currencies[0].code}</p>
+        <p>Dial code: ${country.callingCodes[0]}</p>
+        <img width="300px" src="${country.flag}"></img>
+    `
 }
