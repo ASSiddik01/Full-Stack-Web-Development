c1701b287cab238fa1e933e7788345ab9dfc47ca
Shama <assiddik001@gmail.com>
2021-08-28 01:38:58 +0600

    m-33 v-7: Click and display data mealdb

--- Milestone_6 - Intermediate js, AP/Module_33 - API examplaes with edge cases/05.mealdb.js
+++ Milestone_6 - Intermediate js, AP/Module_33 - API examplaes with edge cases/05.mealdb.js
@@ -14,12 +14,12 @@ const searchFood = () => {
 const displayFood = meals => {
     const searchResult = document.getElementById('search_result');
     meals.forEach(meal => {
-    // console.log(meals);
+    // console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="col">
-          <div class="card h-100">
+          <div onclick="loadFoodDetail(${meal.idMeal})" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${meal.strMeal}</h5>
@@ -30,4 +30,28 @@ const displayFood = meals => {
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadFoodDetail = mealId => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetaile(data.meals[0]));
+}
+
+
+const displayMealDetaile = meal => {
+  console.log(meal);
+  const mealDetailes = document.getElementById('meal_detiles');
+  const div = document.createElement('div');
+  div.classList.add('card')
+  div.innerHTML = `
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${meal.strMeal}</h5>
+    <p class="card-text">${meal.strInstructions.slice(0, 150)}.</p>
+    <a href="${meal.strYoutube}" class="btn btn-primary">Youtube links</a>
+  </div>
+  `;
+  mealDetailes.appendChild(div);
 }
