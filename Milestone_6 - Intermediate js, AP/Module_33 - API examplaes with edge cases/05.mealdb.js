f7fb9e90fb137e9231f1c5880d7a32f55d855cf4
Shama <assiddik001@gmail.com>
2021-08-27 16:20:19 +0600

    m-33 v-6: Display dynamic search result mealdb

--- Milestone_6 - Intermediate js, AP/Module_33 - API examplaes with edge cases/05.mealdb.js
+++ Milestone_6 - Intermediate js, AP/Module_33 - API examplaes with edge cases/05.mealdb.js
@@ -7,5 +7,27 @@ const searchFood = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayFood(data.meals))
+}
+
+
+const displayFood = meals => {
+    const searchResult = document.getElementById('search_result');
+    meals.forEach(meal => {
+    // console.log(meals);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="col">
+          <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">${meal.strInstructions.slice(0,200)}.</p>
+            </div>
+          </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
