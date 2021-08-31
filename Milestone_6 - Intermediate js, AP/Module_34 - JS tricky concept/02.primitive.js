6c43c3a1a7efe90d9b88d94f4b5c067b4b1c98be
Shama <assiddik001@gmail.com>
2021-09-01 00:39:05 +0600

    m-34 v-2: Primitive and Non-premitive data

---
+++ Milestone_6 - Intermediate js, AP/Module_34 - JS tricky concept/02.primitive.js
@@ -0,0 +1,13 @@
+let a = "Hello";
+let b = a;
+console.log(a, b);
+
+a = "Bye"
+console.log(a, b);
+
+let x = { Job: "Web developer" };
+const y = x;
+console.log(x, y);
+y.Job = 'front end developer';
+console.log(x, y);
+
