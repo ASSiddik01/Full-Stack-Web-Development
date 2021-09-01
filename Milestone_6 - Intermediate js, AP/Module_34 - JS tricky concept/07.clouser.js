fb37367d7398258ca6531a9a0b091d5c4bf74fc4
Shama <assiddik001@gmail.com>
2021-09-01 09:04:07 +0600

    m-34 v-7: Closure function

---
+++ Milestone_6 - Intermediate js, AP/Module_34 - JS tricky concept/07.clouser.js
@@ -0,0 +1,15 @@
+function stopWatch() {
+    let counter = 0;
+    return function () {
+        counter++;
+        return counter;
+    }
+}
+
+let clock = stopWatch();
+console.log(clock());
+console.log(clock());
+console.log(clock());
+console.log(clock());
+console.log(clock());
+console.log(clock());
