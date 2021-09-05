d67c28615bcb5f9384ebb274525147335664fb31
Shama <assiddik001@gmail.com>
2021-09-05 12:16:27 +0600

    m-36.5 v-2: Display Data

--- Milestone_6 - Intermediate js, AP/Module_36.5 - API recape/app.js
+++ Milestone_6 - Intermediate js, AP/Module_36.5 - API recape/app.js
@@ -2,7 +2,7 @@ const API_Key = `5ceef2285e57dbf70699e0b127b25e20`;
 
 const searchTemperature = () => {
     const city = document.getElementById('city_name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`;
 
 
     fetch(url)
@@ -10,6 +10,16 @@ const searchTemperature = () => {
         .then(data => displayTemperature(data));
 }
 
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
 const displayTemperature = temperature => {
-    console.log(temperature);
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('weather', temperature.weather[0].main);
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const icon = document.getElementById('icon');
+    icon.setAttribute('src', url);
 }
