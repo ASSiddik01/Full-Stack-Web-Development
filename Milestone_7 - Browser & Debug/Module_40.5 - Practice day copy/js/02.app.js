3830a5861b7bfbd72a0907b15fe8e705cad62893
Shama <assiddik001@gmail.com>
2021-09-13 17:16:22 +0600

    m-41 v-8: Class and private field, JS compile

--- Milestone_7 - Browser & Debug/Module_40.5 - Practice day copy/js/02.app.js
+++ Milestone_7 - Browser & Debug/Module_40.5 - Practice day copy/js/02.app.js
@@ -2,3 +2,21 @@
 const myName = "Abu Shama";
 console.log("hello");
 console.log(myName);
+const fullName = 'Abu Shama Siddik';
+const id = 26;
+function add(first, second) {
+    const result = first + second;
+    return result;
+}
+const output = add(45, 87);
+function doubleConsole(number) {
+    console.log(2 * number);
+}
+// multi purpose
+/* function add(first: number | string, second: number | string): number | string {
+    const result: string | number = first + second;
+    return result;
+}
+const output: number = add(45, 87);
+const fullName = add('Adam', 'Sand');
+ */ 
