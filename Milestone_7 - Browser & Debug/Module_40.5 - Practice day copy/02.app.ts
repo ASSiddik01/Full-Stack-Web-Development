7e11f5f26ea6f9246fc2dac36684e3df0d297a9e
Shama <assiddik001@gmail.com>
2021-09-13 16:48:51 +0600

    m-41 v-4: Set function parameter type

--- Milestone_7 - Browser & Debug/Module_40.5 - Practice day copy/02.app.ts
+++ Milestone_7 - Browser & Debug/Module_40.5 - Practice day copy/02.app.ts
@@ -6,3 +6,26 @@ console.log(myName);
 
 const fullName: string = 'Abu Shama Siddik';
 const id: number = 26;
+
+
+
+function add(first: number, second: number): number {
+    const result = first + second;
+    return result;
+}
+
+const output: number = add(45, 87);
+
+function doubleConsole(number: number): void {
+    console.log(2 * number);
+
+}
+
+// multi purpose
+/* function add(first: number | string, second: number | string): number | string {
+    const result: string | number = first + second;
+    return result;
+}
+const output: number = add(45, 87);
+const fullName = add('Adam', 'Sand');
+ */
