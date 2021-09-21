14393cc45918395cf48a021f3be36aa398d3452c
Shama <assiddik001@gmail.com>
2021-09-21 20:33:29 +0600

    m-45 v-5: Create Multiple Components

--- Milestone_8 - Simple React/Module_45 - JSX, props, stats/core_concept/src/App.js
+++ Milestone_8 - Simple React/Module_45 - JSX, props, stats/core_concept/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 const name = 'Abu Shama';
+const cinema = [
+  {name:'Salman Shah', movie:'Shopner Nayok'},
+  {name:'Riaz', movie:'Hridoyer Kotha'},
+  {name:'Sakib Khan', movie:'Hero'}
+]
 function App() {
   return (
     <div className="App">
@@ -10,9 +15,15 @@ function App() {
       <About></About>
       <About></About> */}
 
-      <Friend name="Shakib" address='Nouga'></Friend>
+      {/* <Friend name="Shakib" address='Nouga'></Friend>
       <Friend name="Salman" address='Rangpur'></Friend>
-      <Friend name="Shamim" address='Noakhali'></Friend>
+      <Friend name="Shamim" address='Noakhali'></Friend> */}
+      
+      {
+        cinema.map(cinema => <Cinema movie={cinema.movie } name={cinema.name } ></Cinema>)
+      }
+
+
     </div>
   );
 }
@@ -25,11 +36,21 @@ function App() {
 //   );
 // }
 
-function Friend(props) {
+// function Friend(props) {
+//   return (
+//     <div className="about">
+//       <h2>Name: {props.name }</h2>
+//       <h4>Address: {props.address }</h4>
+//     </div>
+//   )
+// }
+
+
+function Cinema(props) {
   return (
     <div className="about">
-      <h2>Name: {props.name }</h2>
-      <h4>Address: {props.address }</h4>
+      <h1>Movie name: {props.movie}</h1>
+      <h3>Nayok name: {props.name}</h3>
     </div>
   )
 }
