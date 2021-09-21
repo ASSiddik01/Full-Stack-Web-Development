55fdaff2eea9a14da9091df2eeea27b7c62bf9d4
Shama <assiddik001@gmail.com>
2021-09-21 19:58:48 +0600

    m-45 v-4: Pass Dynamic Data To Components

--- Milestone_8 - Simple React/Module_45 - JSX, props, stats/core_concept/src/App.js
+++ Milestone_8 - Simple React/Module_45 - JSX, props, stats/core_concept/src/App.js
@@ -4,20 +4,33 @@ const name = 'Abu Shama';
 function App() {
   return (
     <div className="App">
+      {/* <About></About>
       <About></About>
       <About></About>
       <About></About>
-      <About></About>
-      <About></About>
+      <About></About> */}
+
+      <Friend name="Shakib" address='Nouga'></Friend>
+      <Friend name="Salman" address='Rangpur'></Friend>
+      <Friend name="Shamim" address='Noakhali'></Friend>
     </div>
   );
 }
-function About() {
+// function About() {
+//   return (
+//     <div className="about">
+//       <h2>Name: {name}</h2>
+//       <h4>Occupetion: Student</h4>
+//     </div>
+//   );
+// }
+
+function Friend(props) {
   return (
     <div className="about">
-      <h2>Name: {name}</h2>
-      <h4>Occupetion: Student</h4>
+      <h2>Name: {props.name }</h2>
+      <h4>Address: {props.address }</h4>
     </div>
-  );
+  )
 }
 export default App;
