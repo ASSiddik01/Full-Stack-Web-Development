ee8b0a92f4b95cd605c70880eddb41bf95d2234c
Shama <assiddik001@gmail.com>
2021-09-23 01:45:30 +0600

    m-45.5 v-2: Check out Event listener

--- Milestone_8 - Simple React/Module_45.5 - Practices day/core_concept_recap/src/App.js
+++ Milestone_8 - Simple React/Module_45.5 - Practices day/core_concept_recap/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -12,16 +13,25 @@ function App() {
 }
 
 function MyComponents(props) {
+  const [points, setPoints]= useState(0)
   const myStyle = {
     backgroundColor: 'lightblue',
-    border: '2px solid yollow',
+    border: '2px solid blue',
     margin: '20px',
-    padding:'20px'
+    padding: '20px',
+    borderRadius: '10px'
+  }
+
+  const addPoints = () => {
+    const newPoints = points + 1;
+    setPoints(newPoints);
   }
   return (
     <div style={myStyle} className="">
       <h1>Brand name: {props.brand }</h1>
-      <h3 style={{color:'red'}}>Price: {props.price }</h3>
+      <h3 style={{ color: 'red' }}>Price: {props.price} </h3>
+      <h3>Rateing: {points }</h3>
+      <button onClick={addPoints }>Add Rateing</button>
     </div>
     
   )
