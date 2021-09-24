b473f5d6622268051c3fb43a9a4b8bf8cb150f1c
Shama <assiddik001@gmail.com>
2021-09-24 07:28:46 +0600

    m47 v4: How states works

--- Milestone_8 - Simple React/Module_47 - How react works/understanding_react/src/App.js
+++ Milestone_8 - Simple React/Module_47 - How react works/understanding_react/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import Device from './components/Device/Device';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [steps, setSteps] = useState(0);
+
+  const increaseWalk = () => {
+    const newSteps = steps + 1;
+    setSteps(newSteps);
+  }
+
+  useEffect(() => {
+    console.log(steps);
+  }, [steps]);
+
   return (
     <div className="App">
+      <h2>My steps: {steps} </h2>
+      <button onClick={increaseWalk }>Walk</button>
       <Device name='iPhone' price='50000'></Device>
     </div>
   );
