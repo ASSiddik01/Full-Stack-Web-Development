bd1df4ca987e5a8f21662132eb29f0df2ff73f8b
Shama <assiddik001@gmail.com>
2021-09-25 22:42:41 +0600

    m48 v3: Load data and set state

--- Milestone_8 - Simple React/Module_48 - Simple react SPA/ema_jhon/src/components/Shop/Shop.js
+++ Milestone_8 - Simple React/Module_48 - Simple react SPA/ema_jhon/src/components/Shop/Shop.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Shop.css'
+import { useState, useEffect } from 'react';
+
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    },[])
     return (
-        <div>
-            <h1>This is shop</h1>
+        <div className='shop_container'>
+            <div className="product_container">
+                <h3>Products:{products.length} </h3>
+                {
+                    products.map(product=>console.log(product))
+                }
+            </div>
+            <div className="cart_container">
+                <h3>Cart</h3>
+                <h5>Order</h5>
+            </div>
+            
         </div>
     );
 };
