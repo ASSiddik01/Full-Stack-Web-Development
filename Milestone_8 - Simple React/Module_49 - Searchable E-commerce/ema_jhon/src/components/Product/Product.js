f5ec1aa4dedcf560f36160caa36e7f4e5b0c9803
Shama <assiddik001@gmail.com>
2021-09-26 11:38:43 +0600

    m49 v7: Display dynamic ratings

--- Milestone_8 - Simple React/Module_49 - Searchable E-commerce/ema_jhon/src/components/Product/Product.js
+++ Milestone_8 - Simple React/Module_49 - Searchable E-commerce/ema_jhon/src/components/Product/Product.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Product.css';
+import Rating from 'react-rating';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />;
 
 const Product = (props) => {
-    const { name, img, seller, price, stock } = props.product;
+    const { name, img, seller, price, stock, star } = props.product;
     return (
         <div className="product">
             <div>
@@ -16,6 +17,13 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>Price: {price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star icon_color"
+                    fullSymbol="fas fa-star icon_color"
+                    readonly
+                ></Rating>
+                <br />
                 <button onClick={()=>props.addToCartHandeler(props.product)} className="btn_regular" >{cartIcon} Add to Cart</button>
             </div>
         </div>
