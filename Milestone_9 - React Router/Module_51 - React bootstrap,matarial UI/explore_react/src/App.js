681f99da80502591858ff6a912c3d951465837ac
Shama <assiddik001@gmail.com>
2021-09-29 22:42:36 +0600

    m51 v2: Install Bootstrap and use

--- Milestone_9 - React Router/Module_51 - React bootstrap,matarial UI/explore_react/src/App.js
+++ Milestone_9 - React Router/Module_51 - React bootstrap,matarial UI/explore_react/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Card from './components/Card/Card';
+import { Button, Spinner } from 'react-bootstrap';
 
 const items = [
   { name: 'Sohid Minar', description: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', img: 'https://thefinancialexpress.com.bd/uploads/1582387861.jpg' },
@@ -19,12 +20,18 @@ const items = [
 
 function App() {
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4">
-      {
-        items.map(item => <Card
-          item={item}
-        ></Card>)
-      }
+    // <div className="row row-cols-1 row-cols-md-3 g-4">
+    //   {
+    //     items.map(item => <Card
+    //       item={item}
+    //     ></Card>)
+    //   }
+    // </div>
+    <div className='App'>
+      <Button variant='primary'>Primary</Button>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
     </div>
   );
 }
