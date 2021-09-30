7f91f3cfe3caf3312827b313ef157bd3f75c1baa
Shama <assiddik001@gmail.com>
2021-09-30 21:38:26 +0600

    m51 v7: Setup tailwind css

---
+++ Milestone_9 - React Router/Module_51 - React bootstrap,matarial UI/tailwind/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-red-700 text-5xl'>This is text</p>
+      <p className='text-custom'>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
