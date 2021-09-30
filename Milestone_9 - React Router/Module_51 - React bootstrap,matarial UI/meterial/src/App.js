1cb5f2d5613925e26dedca5f4cccef548e243552
Shama <assiddik001@gmail.com>
2021-09-30 20:20:19 +0600

    m51 v6: Display News

--- Milestone_9 - React Router/Module_51 - React bootstrap,matarial UI/meterial/src/App.js
+++ Milestone_9 - React Router/Module_51 - React bootstrap,matarial UI/meterial/src/App.js
@@ -1,12 +1,14 @@
 import { Button, Rating } from '@mui/material';
 
 import './App.css';
+import Newspaper from './components/Newspaper/Newspaper';
 
 function App() {
   return (
     <div className="App">
       <Button variant="contained">Contained</Button>
       <Rating name="read-only" value={2} readOnly />
+      <Newspaper></Newspaper>
     </div>
   );
 }
