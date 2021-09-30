749978b29699473a2775cb3a5a22097f4169b7a4
Shama <assiddik001@gmail.com>
2021-10-01 00:14:54 +0600

    m51 v8: Use reachart

---
+++ Milestone_9 - React Router/Module_51 - React bootstrap,matarial UI/rechart/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Line_Chart from './components/LineChart/LineChart';
+
+function App() {
+  return (
+    <div className="App">
+      <Line_Chart></Line_Chart>
+    </div>
+  );
+}
+
+export default App;
