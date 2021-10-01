f577409ef03e2823a21ea1ea4bc0fb04d738613f
Shama <assiddik001@gmail.com>
2021-10-01 14:56:55 +0600

    m52 v4: Recap Router

--- Milestone_9 - React Router/Module_52 - Raact Router/react_router/src/App.js
+++ Milestone_9 - React Router/Module_52 - Raact Router/react_router/src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
+import Home from './Components/Home/Home';
 import Friends from './Components/Friends/Friends';
+import About from './Components/About/About';
+import NotFound from './Components/NotFound/NotFound';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 
 function App() {
   return (
     <div className="App">
-      <Friends></Friends>
+      <Router>
+        <Switch>
+          <Route path='/home'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='/about'>
+            <About></About>
+          </Route>
+          <Route path='/friends'>
+            <Friends></Friends>
+          </Route>
+          <Route>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
