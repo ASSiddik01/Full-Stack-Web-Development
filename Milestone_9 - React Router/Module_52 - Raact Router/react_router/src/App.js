c13e62767aceaf8defea5c462db7255d4cebdf9a
Shama <assiddik001@gmail.com>
2021-10-01 11:43:33 +0600

    m52 v2: imple router setup

--- Milestone_9 - React Router/Module_52 - Raact Router/react_router/src/App.js
+++ Milestone_9 - React Router/Module_52 - Raact Router/react_router/src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
-import Home from './Components/Home/Home';
-import Friends from './Components/Friends/Friends';
-import About from './Components/About/About';
-import NotFound from './Components/NotFound/NotFound';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import NotFound from './Components/NotFound/NotFound'
+
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
-      <NotFound/>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/home'>
+            <Home></Home>
+          </Route>
+          <Route path='/about'>
+            <About></About>
+          </Route>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
