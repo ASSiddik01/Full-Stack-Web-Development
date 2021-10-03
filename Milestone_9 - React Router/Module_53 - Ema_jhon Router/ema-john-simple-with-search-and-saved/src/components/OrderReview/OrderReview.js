aad180325422072a36413c90711d2881693a511e
Shama <assiddik001@gmail.com>
2021-10-03 15:18:43 +0600

    m53 v4: Create useCart and use it

--- Milestone_9 - React Router/Module_53 - Ema_jhon Router/ema-john-simple-with-search-and-saved/src/components/OrderReview/OrderReview.js
+++ Milestone_9 - React Router/Module_53 - Ema_jhon Router/ema-john-simple-with-search-and-saved/src/components/OrderReview/OrderReview.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const OrderReview = () => {
     const [products] = useProducts();
+    const [cart] = useCart(products);
     return (
         <div>
             <h1>Total product:{products.length} </h1>
+            <h2>Cart: {cart.length} </h2>
             <h2>This is Order Review</h2>
         </div>
     );
