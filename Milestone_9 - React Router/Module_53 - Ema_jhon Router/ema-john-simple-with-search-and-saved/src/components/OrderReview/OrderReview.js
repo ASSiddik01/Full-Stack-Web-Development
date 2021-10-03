b1227e659a50f54c03dcb486a876a416d94f8823
Shama <assiddik001@gmail.com>
2021-10-03 16:34:10 +0600

    m53 v6: Remove Item handler

--- Milestone_9 - React Router/Module_53 - Ema_jhon Router/ema-john-simple-with-search-and-saved/src/components/OrderReview/OrderReview.js
+++ Milestone_9 - React Router/Module_53 - Ema_jhon Router/ema-john-simple-with-search-and-saved/src/components/OrderReview/OrderReview.js
@@ -2,15 +2,27 @@ import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
     const [products] = useProducts();
-    const [cart] = useCart(products);
+    const [cart, setCart] = useCart(products);
+    const removehandler = (key) => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
-            <div className="product-container"></div>
+            <div className="product-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        removehandler={removehandler}
+                        product={product} ></ReviewItem>)
+                }
+            </div>
             <div className="cart-container">
-                <Cart cart={cart }/>
+                <Cart cart={cart} />
             </div>
         </div>
     );
