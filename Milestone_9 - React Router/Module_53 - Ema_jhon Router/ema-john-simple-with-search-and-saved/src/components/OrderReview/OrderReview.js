7852d904acee6567312c24532760694044ed5167
Shama <assiddik001@gmail.com>
2021-10-03 15:35:10 +0600

    m53 v5: Display Shopping cart

--- Milestone_9 - React Router/Module_53 - Ema_jhon Router/ema-john-simple-with-search-and-saved/src/components/OrderReview/OrderReview.js
+++ Milestone_9 - React Router/Module_53 - Ema_jhon Router/ema-john-simple-with-search-and-saved/src/components/OrderReview/OrderReview.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
 
 const OrderReview = () => {
     const [products] = useProducts();
     const [cart] = useCart(products);
     return (
-        <div>
-            <h1>Total product:{products.length} </h1>
-            <h2>Cart: {cart.length} </h2>
-            <h2>This is Order Review</h2>
+        <div className='shop-container'>
+            <div className="product-container"></div>
+            <div className="cart-container">
+                <Cart cart={cart }/>
+            </div>
         </div>
     );
 };
