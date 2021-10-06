df42c5faae87f7c1b2e42e5e35e06f356b18b1fa
Shama <assiddik001@gmail.com>
2021-10-06 17:31:07 +0600

    m56 v4: Sign in with google

--- Milestone_10 - React Authentication/Module_56 - Firebase Authentication/simple_firebase_authentication/src/App.js
+++ Milestone_10 - React Authentication/Module_56 - Firebase Authentication/simple_firebase_authentication/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import {getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import './App.css';
+import inatializaAutentication from './FireBase/firebase.inatialize';
+
+inatializaAutentication();
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const signInHandeler = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+    })
+  }
   return (
     <div className="App">
-      
+      <button onClick={signInHandeler}>Google Sing In</button>
     </div>
   );
 }
