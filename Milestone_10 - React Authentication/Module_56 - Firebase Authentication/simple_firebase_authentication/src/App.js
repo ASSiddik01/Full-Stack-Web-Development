f4de88659a3c95a49f2381bf9911e17f2270242d
Shama <assiddik001@gmail.com>
2021-10-06 18:33:04 +0600

    m56 v6: Display user info

--- Milestone_10 - React Authentication/Module_56 - Firebase Authentication/simple_firebase_authentication/src/App.js
+++ Milestone_10 - React Authentication/Module_56 - Firebase Authentication/simple_firebase_authentication/src/App.js
@@ -1,22 +1,42 @@
-import {getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import './App.css';
 import inatializaAutentication from './FireBase/firebase.inatialize';
+import { useState } from 'react';
 
 inatializaAutentication();
 const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState([])
+
   const signInHandeler = () => {
     const auth = getAuth();
     signInWithPopup(auth, provider)
       .then(result => {
-        const user = result.user;
-        console.log(user);
-    })
+        const { displayName, email, photoURL } = result.user;
+        console.log(photoURL)
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        }
+        setUser(loggedInUser);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
   }
   return (
     <div className="App">
       <button onClick={signInHandeler}>Google Sing In</button>
+      <br />
+      {
+        user.email && <div>
+          <h2>Welcome {user.name} </h2>
+          <p>Your email: {user.email} </p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
