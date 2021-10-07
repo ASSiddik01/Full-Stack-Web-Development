5578f333553c3320fddc509a7f9c16fc763d60f5
Shama <assiddik001@gmail.com>
2021-10-07 09:46:44 +0600

    m57 v4: Create New User

--- Milestone_10 - React Authentication/Module_57 - Email Authentication/firebase_email_auth/src/App.js
+++ Milestone_10 - React Authentication/Module_57 - Email Authentication/firebase_email_auth/src/App.js
@@ -1,26 +1,53 @@
 import './App.css';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import inatializaAutentication from "./Firebase/firebase.inatialize"
+import { useState } from 'react';
+
+inatializaAutentication()
+const auth = getAuth();
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+
+  // Submit handle
   const handleSignUp = e => {
-    console.log('Sign Up');
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+    })
     e.preventDefault();
+  }
+  // Email handle
+  const handleEmail = e => {
+    setEmail(e.target.value);
+  }
+  // Password handle
+  const handlePassword = e => {
+    setPassword(e.target.value);
   }
 
 
 
   return (
     <div className="App">
-
       <form onSubmit={handleSignUp} >
         <h1>Please! Sign Up</h1>
-        <label htmlFor="email">Email: </label>
-        <input type="email" name="email" />
-        <br />
-        <label htmlFor="password">Password: </label>
-        <input type="password" name="password" />
-        <br />
-        <br />
-        <input type="submit" value="Sign Up" />
+        <div className="row mb-3">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email:</label>
+          <div className="col-sm-10">
+            <input onBlur={handleEmail} type="email" className="form-control" id="inputEmail3" />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input onBlur={handlePassword} type="password" className="form-control" id="inputPassword3" />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Sign Up</button>
       </form>
     </div>
   );
---
+++ Milestone_10 - React Authentication/Module_57 - Email Authentication/firebase_email_auth/src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAflGFIGaYhfodDMncqiwgbZTo7NOw7Yyk",
+    authDomain: "email-auth-68b09.firebaseapp.com",
+    projectId: "email-auth-68b09",
+    storageBucket: "email-auth-68b09.appspot.com",
+    messagingSenderId: "908005036586",
+    appId: "1:908005036586:web:4acd5a6c3b7cbf9be86953"
+};
+  
+export default firebaseConfig;
