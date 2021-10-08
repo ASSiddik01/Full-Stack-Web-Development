42f4dbb4b73676b0b1b4893e87450c4a37c61d5c
Shama <assiddik001@gmail.com>
2021-10-08 15:32:35 +0600

    ready for practice

---
+++ Milestone_10 - React Authentication/Module_57.5 - Firebase Practice/firebase_practice/src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCU-9sbrTLJEnshu_USJJcs5nwguDe3qrc",
+    authDomain: "fir-practice-c12b9.firebaseapp.com",
+    projectId: "fir-practice-c12b9",
+    storageBucket: "fir-practice-c12b9.appspot.com",
+    messagingSenderId: "91327618601",
+    appId: "1:91327618601:web:802bf279292a8967a78638"
+  };
+
+
+export default firebaseConfig;
