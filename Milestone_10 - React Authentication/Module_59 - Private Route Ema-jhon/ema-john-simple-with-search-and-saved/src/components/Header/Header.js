13fd7d32c62a460e95e760a04319bb4ba14f453d
Shama <assiddik001@gmail.com>
2021-10-14 23:30:23 +0600

    m59 v5: Use Auth Context

--- Milestone_10 - React Authentication/Module_59 - Private Route Ema-jhon/ema-john-simple-with-search-and-saved/src/components/Header/Header.js
+++ Milestone_10 - React Authentication/Module_59 - Private Route Ema-jhon/ema-john-simple-with-search-and-saved/src/components/Header/Header.js
@@ -2,10 +2,10 @@ import React from 'react';
 import logo from '../../images/logo.png';
 import './Header.css';
 import { NavLink } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import useAuth from '../../hooks/useAuth';
 
 const Header = () => {
-    const { user, logOut } = useFirebase();
+    const { user, logOut } = useAuth();
     return (
         <div className="header">
             <img className="logo" src={logo} alt="" />
@@ -13,6 +13,7 @@ const Header = () => {
                 <NavLink to="/shop">Shop</NavLink>
                 <NavLink to="/review">Order Review</NavLink>
                 <NavLink to="/inventory">Manage Inventory</NavLink>
+                {user.displayName && <span style={{ color: 'white' }} >user: {user.displayName} </span>}
                 {user.email ?
                     <button onClick={logOut}>Sign Out</button>
                     :
