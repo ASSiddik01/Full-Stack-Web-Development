988f4368d593fb6a0c66dc2b4d2738a1424e2efb
Shama <assiddik001@gmail.com>
2021-10-16 03:11:51 +0600

    m59 v9: Create shipment form

--- Milestone_10 - React Authentication/Module_59 - Private Route Ema-jhon/ema-john-simple-with-search-and-saved/src/components/Shipping/Shipping.js
+++ Milestone_10 - React Authentication/Module_59 - Private Route Ema-jhon/ema-john-simple-with-search-and-saved/src/components/Shipping/Shipping.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import useAuth from '../../hooks/useAuth';
+import './Shipping.css'
 
 const Shipping = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { user } = useAuth();
+    const onSubmit = data => {
+        console.log(data)
+    };
     return (
-        <div>
-            <h2>Shipping</h2>
+        <div className="shipping_form" >
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input placeholder="Your Name" defaultValue={user.displayName} {...register("name")} />
+                <input placeholder="Your Email" defaultValue={user.email} {...register("email", { required: true })} />
+                {errors.email && <span className='error'>This field is required</span>}
+                <input placeholder="Your Adress" defaultValue="" {...register("address")} />
+                <input placeholder="Your City" defaultValue="" {...register("city")} />
+                <input placeholder="Your Phone" defaultValue="" {...register("phone")} />
+                
+                <input type="submit" />
+            </form>
         </div>
     );
 };
