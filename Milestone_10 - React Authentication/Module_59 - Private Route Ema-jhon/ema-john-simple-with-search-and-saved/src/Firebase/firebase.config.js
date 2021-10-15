efb3d73cf98fabc366b3695bda87af3fa579fdf8
Shama <assiddik001@gmail.com>
2021-10-15 23:11:52 +0600

    m59 v1: Project Setup

---
+++ Milestone_10 - React Authentication/Module_59 - Private Route Ema-jhon/ema-john-simple-with-search-and-saved/src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxx4O1S43R7KX40DShQqRQMA47OfeoHRk",
+    authDomain: "ema-jhon-25df0.firebaseapp.com",
+    projectId: "ema-jhon-25df0",
+    storageBucket: "ema-jhon-25df0.appspot.com",
+    messagingSenderId: "267470915928",
+    appId: "1:267470915928:web:02665542e173395f597d2d"
+};
+
+export default firebaseConfig;
