10ab890cd95f86c3b08fd89c56565388b11b7b3b
Shama <assiddik001@gmail.com>
2021-10-17 01:41:14 +0600

    m60 v4: Image collect and hosting in ImgDB

---
+++ Milestone_10 - React Authentication/Module_60 - Responsive React Website/respnsive_react_website/src/Pages/Home/Services/Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import service_1 from '../../../images/services/1.jpg';
+
+const services = [
+    {name: 'Replace Tire', price: 2000, time: 2, img:service_1}
+]
+
+const Services = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Services;
