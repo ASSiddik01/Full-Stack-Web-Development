ca2d9773d2d915cf2e6597f83a1622d2f55927f8
Shama <assiddik001@gmail.com>
2021-10-17 22:34:10 +0600

    m61 v4: Make Header and create Firebase

---
+++ Milestone_10 - React Authentication/Module_61 - React Router and Firebase Recap/genious_car_website/src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBfZT_C3iydFym_oHjU9DFAhEQXysPEzW8",
+    authDomain: "genius-mechanics-de64b.firebaseapp.com",
+    projectId: "genius-mechanics-de64b",
+    storageBucket: "genius-mechanics-de64b.appspot.com",
+    messagingSenderId: "982931752893",
+    appId: "1:982931752893:web:63a40bb0afd7b4136734d4"
+};
+  
+export default firebaseConfig;
