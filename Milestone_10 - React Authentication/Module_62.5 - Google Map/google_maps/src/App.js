291b0f90d9d8722dfb0b2c17dbd022b9795e4b54
Shama <assiddik001@gmail.com>
2021-10-23 08:29:22 +0600

    m62.5 v3: Dynamically Search And Display route

--- Milestone_10 - React Authentication/Module_62.5 - Google Map/google_maps/src/App.js
+++ Milestone_10 - React Authentication/Module_62.5 - Google Map/google_maps/src/App.js
@@ -1,11 +1,29 @@
+import { useRef, useState } from 'react';
 import './App.css';
-import Map from './components/Map/Map';
 import Direction from './components/Direction/Direction';
+import Map from './components/Map/Map';
 
 function App() {
+  const [origin, setOrigin] = useState('');
+  const [destination, setDestination] = useState('');
+  const originRef = useRef('');
+  const destinationRef = useRef('');
+
+  const handleDirection = e => {
+    setOrigin(originRef.current.value);
+    setDestination(destinationRef.current.value);
+    e.preventDefault();
+  }
   return (
     <div className="App">
-      <Direction/>
+      <form onSubmit={handleDirection}>
+        <input type="text" ref={originRef} placeholder="Start from" />
+        <br />
+        <input type="text" ref={destinationRef} placeholder="Destination" />
+        <br />
+        <input type="submit" value="Get Direction" />
+      </form>
+      <Direction origin={origin} destination={destination}></Direction>
     </div>
   );
 }
