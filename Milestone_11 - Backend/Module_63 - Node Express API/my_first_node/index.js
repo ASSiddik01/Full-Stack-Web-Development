b1b797646a07cd3b1d1a6aec53b78d30e827a238
Shama <assiddik001@gmail.com>
2021-10-24 21:07:03 +0600

    m63 v3: Recap Node express and install nodemon

--- Milestone_11 - Backend/Module_63 - Node Express API/my_first_node/index.js
+++ Milestone_11 - Backend/Module_63 - Node Express API/my_first_node/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('hello Shama, welcome to USA');
 })
 
 app.listen(port, () => {
