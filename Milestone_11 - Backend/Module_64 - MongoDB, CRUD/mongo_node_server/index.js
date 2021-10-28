a59bce88f8572dd1cee062fd5d713b8c78d9ead6
Shama <assiddik001@gmail.com>
2021-10-28 09:32:35 +0600

    m64 v5: Setup client side

--- Milestone_11 - Backend/Module_64 - MongoDB, CRUD/mongo_node_server/index.js
+++ Milestone_11 - Backend/Module_64 - MongoDB, CRUD/mongo_node_server/index.js
@@ -6,19 +6,25 @@ const port = 5000;
 
 const uri = "mongodb+srv://Shama:8BallPull@cluster0.i5p5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-    console.log('Mongo DB');
-    const user = { name: "Siddik", email: "siddik@gmail.com", phone: "0199999999999" };
-    collection.insertOne(user)
-    .then(() => {
-        console.log('Data Send');
-    })
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("mydata");
+      const haiku = database.collection("alluser");
+      
+      // Post api
+      app.post('/users', async (req, res) => {
+        console.log('Hitting the post');
+        res.send('hit the post');
+      })
+        
+        
 
-
-//   client.close();
-});
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running my CRUD Server');
