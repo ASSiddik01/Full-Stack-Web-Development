6534971b70db3009f0d199244c1b97f561037af4
Shama <assiddik001@gmail.com>
2021-10-29 19:36:23 +0600

    m65 v3: Connect to database

--- Milestone_11 - Backend/Module_65 - Genius Car Mongo DB/Genius_Car_Mechanics_Server/index.js
+++ Milestone_11 - Backend/Module_65 - Genius Car Mongo DB/Genius_Car_Mechanics_Server/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+require('dotenv').config()
 
 const app = express();
 const port = 5000;
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.i5p5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i5p5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
+async function run() {
+    try {
+        await client.connect();
+        console.log('DB connect')
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
