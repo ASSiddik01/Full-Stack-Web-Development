0872e939611495055e548feaea5080d230b5870e
Shama <assiddik001@gmail.com>
2021-10-29 21:55:00 +0600

    m65 v5: Use React hook form

--- Milestone_11 - Backend/Module_65 - Genius Car Mongo DB/Genius_Car_Mechanics/src/Pages/AddServices/AddServices.js
+++ Milestone_11 - Backend/Module_65 - Genius Car Mongo DB/Genius_Car_Mechanics/src/Pages/AddServices/AddServices.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddServices.css';
 
 const AddServices = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
     return (
-        <div>
+        <div className='add_service'>
             <h1>Add a services</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input placeholder="Name" {...register("name")} />
+                <textarea placeholder="Description" {...register("description")} />
+                <input placeholder="Image url" {...register("img")} />
+                <input placeholder="price" type="number" {...register("price")} />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
