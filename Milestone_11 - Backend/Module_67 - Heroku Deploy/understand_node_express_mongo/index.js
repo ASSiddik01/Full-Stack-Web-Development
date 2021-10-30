ca39a158d31ecfcc77aca1b6091844a4a39080b9
Shama <assiddik001@gmail.com>
2021-10-30 13:36:27 +0600

    m67 v3: Steps to connect to Mongodb

--- Milestone_11 - Backend/Module_67 - Heroku Deploy/understand_node_express_mongo/index.js
+++ Milestone_11 - Backend/Module_67 - Heroku Deploy/understand_node_express_mongo/index.js
@@ -10,8 +10,30 @@
 // ---------------
 
 const express = require('express')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+
 const app = express()
-const port = 7000
+const port = 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i5p5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('DB Connected')
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
