7225b9f3ac67350ae1df47f75077289bc080e037
Shama <assiddik001@gmail.com>
2021-10-30 13:13:53 +0600

    m67 v2: Understanding express server

--- Milestone_11 - Backend/Module_67 - Heroku Deploy/understand_node_express_mongo/index.js
+++ Milestone_11 - Backend/Module_67 - Heroku Deploy/understand_node_express_mongo/index.js
@@ -1,7 +1,22 @@
-const os = require('os');
+// const os = require('os');
 
-console.log('OS Version',os.version())
-console.log('OS BIT',os.arch())
+// console.log('OS Version',os.version())
+// console.log('OS BIT',os.arch())
 
 
-console.log('Running Node')
+// console.log('Running Node')
+
+// Video -2:
+// ---------------
+
+const express = require('express')
+const app = express()
+const port = 7000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
